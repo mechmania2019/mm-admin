116b9d8896b8c9d808ecbe7ec56a51ae3be5d321
angiecheng <angie.cheng33@gmail.com>
2019-09-19 19:34:35 -0500

    removed micro and router services

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const { promisify } = require("util");
-
 const mongoose = require("mongoose");
 const authenticate = require("mm-authenticate")(mongoose);
-const { send } = require("micro");
-const { router, get } = require("microrouter");
 const { Team, Match } = require("mm-schemas")(mongoose);
-const fetch = require("node-fetch");
 
-mongoose.connect(process.env.MONGO_URL);
+const send = (res, status, data) => (res.statusCode = status, res.end(data));
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 const getMatchKey = (me, other) => {
@@ -16,12 +13,15 @@ const getMatchKey = (me, other) => {
 };
 
 module.exports = authenticate(
-  router(
-    get("/leaderboard", async (req, res) => {
+  async (req, res ) => {
+    console.log(req.url);
+    if (req.url.startsWith("/leaderboard")) {
       const team = req.user;
       if (!team.admin) {
-        return send(res, 401, "Unauthorized");
+        send (res, 401, "Unauthorized");
+        return;
       }
+      
       console.log("Getting teams");
       const teams = await Team.find({})
         .populate("latestScript")
@@ -30,65 +30,144 @@ module.exports = authenticate(
         .filter(t => t.latestScript)
         .map(t => t.latestScript.key);
 
-      const scoreMap = {};
-      teams.filter(t => t.latestScript).forEach(t => {
-        scoreMap[t.latestScript.key] = {
-          team: t,
-          wins: 0,
-          losses: 0,
-          ties: 0,
-          score: 0,
-          left: []
-        };
-      });
+        const scoreMap = {};
+        teams.filter(t => t.latestScript).forEach(t => {
+          scoreMap[t.latestScript.key] = {
+            team: t,
+            wins: 0,
+            losses: 0,
+            ties: 0,
+            score: 0,
+            left: []
+          };
+        });
 
-      let { matches } = scripts.reduce(
-        (p, c) => {
-          p.opponents.forEach(o => {
-            p.matches.push(getMatchKey(c, o));
-          });
-          p.opponents.push(c);
-          return p;
-        },
-        {
-          opponents: [],
-          matches: []
-        }
-      );
+        let { matches } = scripts.reduce(
+          (p, c) => {
+            p.opponents.forEach(o => {
+              p.matches.push(getMatchKey(c, o));
+            });
+            p.opponents.push(c);
+            return p;
+          },
+          {
+            opponents: [],
+            matches: []
+          }
+        );
 
-      matches = await Promise.all(
-        matches.map(async key => ({
-          key,
-          match: await Match.findOne({ key }).exec()
-        }))
-      );
+        matches = await Promise.all(
+          matches.map(async key => ({
+            key,
+            match: await Match.findOne({ key }).exec()
+          }))
+        );
 
-      matches.forEach(({ key, match }) => {
-        const [p1, p2] = key.slice("logs/".length).split(":");
-        if (!match) {
-          scoreMap[p1].left.push(p2);
-          scoreMap[p2].left.push(p1);
-          return;
-        }
-        console.log(p1, p2, match.winner);
-        if (match.winner === 3) {
-          scoreMap[p1].ties++;
-          scoreMap[p1].score++;
-          scoreMap[p2].ties++;
-          scoreMap[p2].score++;
-        } else if (match.winner === 2) {
-          scoreMap[p1].losses++;
+        matches.forEach(({ key, match }) => {
+          const [p1, p2] = key.slice("logs/".length).split(":");
+          if (!match) {
+            scoreMap[p1].left.push(p2);
+            scoreMap[p2].left.push(p1);
+            return;
+          }
 
-          scoreMap[p2].wins++;
-          scoreMap[p2].score += 3;
-        } else {
-          scoreMap[p2].losses++;
+          console.log(p1, p2, match.winner);
+          if (match.winner === 0) {
+            scoreMap[p1].ties++;
+            scoreMap[p1].score++;
+            scoreMap[p2].ties++;
+            scoreMap[p2].score++;
+          } else if (match.winner === 2) {
+            scoreMap[p1].losses++;
+            scoreMap[p2].wins++;
+            scoreMap[p2].score += 3;
+          } else {
+            scoreMap[p2].losses++;
+            scoreMap[p1].wins++;
+            scoreMap[p1].score +=3;
+          }
+        });
+        send(res, 200, JSON.stringify({ scores: scoreMap }));
+    }
+  }
+)
 
-          scoreMap[p1].wins++;
-          scoreMap[p1].score += 3;
-        }
-      });
-      return send(res, 200, { scores: scoreMap });
-    })
-  )
-);
+
+// module.exports = authenticate(
+//   router(
+//     get("/leaderboard", async (req, res) => {
+//       const team = req.user;
+//       if (!team.admin) {
+//         send(res, 401, "Unauthorized");
+//         return;
+//       }
+//       console.log("Getting teams");
+//       const teams = await Team.find({})
+//         .populate("latestScript")
+//         .exec();
+//       const scripts = teams
+//         .filter(t => t.latestScript)
+//         .map(t => t.latestScript.key);
+
+//       const scoreMap = {};
+//       teams.filter(t => t.latestScript).forEach(t => {
+//         scoreMap[t.latestScript.key] = {
+//           team: t,
+//           wins: 0,
+//           losses: 0,
+//           ties: 0,
+//           score: 0,
+//           left: []
+//         };
+//       });
+
+//       let { matches } = scripts.reduce(
+//         (p, c) => {
+//           p.opponents.forEach(o => {
+//             p.matches.push(getMatchKey(c, o));
+//           });
+//           p.opponents.push(c);
+//           return p;
+//         },
+//         {
+//           opponents: [],
+//           matches: []
+//         }
+//       );
+
+//       matches = await Promise.all(
+//         matches.map(async key => ({
+//           key,
+//           match: await Match.findOne({ key }).exec()
+//         }))
+//       );
+
+//       matches.forEach(({ key, match }) => {
+//         const [p1, p2] = key.slice("logs/".length).split(":");
+//         if (!match) {
+//           scoreMap[p1].left.push(p2);
+//           scoreMap[p2].left.push(p1);
+//           return;
+//         }
+//         console.log(p1, p2, match.winner);
+//         if (match.winner === 3) {
+//           scoreMap[p1].ties++;
+//           scoreMap[p1].score++;
+//           scoreMap[p2].ties++;
+//           scoreMap[p2].score++;
+//         } else if (match.winner === 2) {
+//           scoreMap[p1].losses++;
+
+//           scoreMap[p2].wins++;
+//           scoreMap[p2].score += 3;
+//         } else {
+//           scoreMap[p2].losses++;
+
+//           scoreMap[p1].wins++;
+//           scoreMap[p1].score += 3;
+//         }
+//       });
+//       send(res, 200, JSON.stringify({ scores: scoreMap }));
+//     })
+//   )
+// );
